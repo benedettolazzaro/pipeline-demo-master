32c5d9c87d20060f4599de2172208e3730ef176b
benedettolazzaro <64127494+benedettolazzaro@users.noreply.github.com>
2020-04-28 14:23:59 +0200

    fix controller v2

--- src/main/java/it/corso/esempio/controller/HelloController3.java
+++ src/main/java/it/corso/esempio/controller/HelloController3.java
@@ -2,7 +2,6 @@ package it.corso.esempio.controller;
 
 import it.corso.esempio.pojo.HelloResponse;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
